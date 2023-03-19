ead6f35979db6d0525466faccc1d31b3771624d8
MZdzw <mar.zdzieblowski.98@gmail.com>
2023-03-19 13:34:41 +0100

    Added reading from file CPU statistics in readerThread

--- inc/readerThr.h
+++ inc/readerThr.h
@@ -1,6 +1,24 @@
 #ifndef READERTHR_H
 #define READERTHR_H
 
+#define OK 1
+#define FILE_OPEN_ERROR -1
+#define FILE_READ_ERROR -2
+
+struct CpuUsageStats
+{
+    unsigned long t_user;
+    unsigned long t_nice;
+    unsigned long t_system;
+    unsigned long t_idle;
+    unsigned long t_iowait;
+    unsigned long t_irq;
+    unsigned long t_softirq;
+    unsigned long t_steal;
+    unsigned long t_guest;
+    unsigned long t_guestNice;
+};
+
 void* readerThread(void* arg);
 
 #endif
--- src/readerThr.c
+++ src/readerThr.c
@@ -1,14 +1,67 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include "readerThr.h"
+
+#define MAX 80
+
+static int getCpuStatsFromFile();
+
+static char buf[MAX];
+struct CpuUsageStats cpuStats;
+unsigned long* ptrCpyStatsLookUpTable[10] = {
+ &(cpuStats.t_user), &(cpuStats.t_nice), &(cpuStats.t_system), &(cpuStats.t_idle),
+ &(cpuStats.t_iowait), &(cpuStats.t_irq), &(cpuStats.t_softirq), 
+ &(cpuStats.t_steal), &(cpuStats.t_guest), &(cpuStats.t_guestNice)
+ };
+
 
 void* readerThread(void* arg)
 {
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 20; i++)
     {
         sleep(1);
         printf("Reader Thread\n");
+
+        if(getCpuStatsFromFile() != OK)
+            return NULL;
     }
+
     return NULL;
+}
+
+static int getCpuStatsFromFile()
+{
+    FILE* ptrStatsFile = fopen("/proc/stat", "r");   //open in read-only mode stat file
+    if(ptrStatsFile == NULL)
+    {
+        perror("Could not open stat file");
+        fclose(ptrStatsFile);
+        return FILE_OPEN_ERROR;      //return error
+    }
+    if(fgets(buf, MAX, ptrStatsFile) == NULL)
+    {
+        fclose(ptrStatsFile);
+        return FILE_READ_ERROR;
+    }
+
+    char* ptrStringSplit = strtok(buf + 3, " ");   //"CPU \0 92324..."
+                                        // 012 3  456
+    
+    char* ptrTmp;
+    size_t cnt = 0;
+    while(ptrStringSplit != NULL)
+	{
+        if(cnt < 10)
+        {
+            *ptrCpyStatsLookUpTable[cnt++] = strtoul(ptrStringSplit, &ptrTmp, 10);
+            ptrStringSplit = strtok(NULL, " ");
+        }
+	}
+    printf("%lu\n", cpuStats.t_user);
+
+    fclose(ptrStatsFile);
+    return OK;
 }
